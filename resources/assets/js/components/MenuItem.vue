35cde047d2bf70e8d6b00289202e5736f7509e00
Jaggy Gauran <jaggy@artisan.studio>
2018-06-10 11:00:17 +0800

    Fix the menu item hovers and remove the CSS from the file.

--- resources/assets/js/components/MenuItem.vue
+++ resources/assets/js/components/MenuItem.vue
@@ -1,20 +1,5 @@
 <template>
-<li class="[ px-8 py-4 whitespace-no-wrap hover:bg-grey-lightest ]">
-  <slot></slot>
-</li>
+    <li class="card-menu-li">
+        <slot></slot>
+    </li>
 </template>
-
-<script>
-export default {
-}
-</script>
-
-<style scoped>
-li a, li button, li form {
-  display: block;
-}
-
-li a {
-  text-decoration: none;
-}
-</style>
