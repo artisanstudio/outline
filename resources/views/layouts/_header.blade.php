72a64e7785631798a20cf38c3337444b5a492fcf
Jaggy Gauran <jaggy@artisan.studio>
2018-09-15 16:22:45 +0800

    Remove the useless fancy schmancy navigator.

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,13 +1,4 @@
 <header class="[ py-4 px-6 md:px-8 flex ]">
-    <nav class="browser-actions [ inline-flex ]">
-        <a class="button [ rounded-r-none p-1 ]" href="#">
-            <i class="icon icon-chevron-left [ ms-lg ]"></i>
-        </a>
-        <a class="button [ border-l-0 p-1 rounded-l-none ]" href="#">
-            <i class="icon icon-chevron-right [ ms-lg ]"></i>
-        </a>
-    </nav>
-
     <a href="#" class="button [ py-1 px-2 ml-auto md:hidden ]" @click.prevent="openSidebar">
         <i class="icon icon-menu"></i>
     </a>
