dcfb4922c62050edbd2717db0436d6a1da11d2be
Jaggy Gauran <jaggy@artisan.studio>
2018-10-03 04:23:15 +0800

    Add a customizable header via partials.

--- resources/views/layouts/_sidebar.blade.php
+++ resources/views/layouts/_sidebar.blade.php
@@ -1,11 +1,11 @@
 <aside class="app-sidebar" :class="{ '-open': sidebar.open }">
-    <h1 class="title">Outline Admin</h1>
+    @include('outline::partials.logo')
 
     <nav class="navigation sidebar-navigation mb-4">
         <ul class="links">
             @foreach($sidebar as $item)
             <li class="item {{ Facades\Artisan\Outline\Outline::activeWhen(array_get($item, 'scope', $item['path'])) ? '-active' : null }}">
-                <a class="button -left -transparent -with-icon" href="{{ $item['path'] }}">
+                <a class="button justify-start -transparent -with-icon" href="{{ $item['path'] }}">
                     <i class="icon icon-{{ $item['icon'] }} [ mr-2 ]" style="margin-top: 1.5px; opacity: .9;"></i>
                     {{ $item['title'] }}
                 </a>
