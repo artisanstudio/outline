1f2ae908bd1472d462f3f28b949e8b5bc713c53f
Jaggy Gauran <jaggy@artisan.studio>
2018-10-03 17:25:10 +0800

    Add a wrapper to support fields with icons.

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,7 +1,16 @@
 <header class="app-header">
-    <section class="global-search form-field">
-        <input class="input" name="" type="text" placeholder="Search everywhere...">
-    </section>
+    <div class="global-search">
+        <form action="" method="GET">
+            <div class="form-field -with-icon">
+                <div class="control">
+                    <label class="icon" for="search">
+                        <i class="icon icon-search"></i>
+                    </label>
+                    <input id="search" name="q" class="input" placeholder="Search everywhere...">
+                </div>
+            </div>
+        </form>
+    </div>
 
     <a href="#" class="hamburger button" @click.prevent="openSidebar">
         <i class="icon icon-menu"></i>
