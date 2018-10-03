1c909522b88b79255791b3341922d02e0ceba60d
Jaggy Gauran <jaggy@artisan.studio>
2018-10-03 14:13:43 +0800

    Add a search placeholder.

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,4 +1,8 @@
 <header class="app-header">
+    <section class="global">
+        <input class="border p-2 px-4 text-14" name="" type="text" placeholder="Search everywhere...">
+    </section>
+
     <a href="#" class="hamburger button" @click.prevent="openSidebar">
         <i class="icon icon-menu"></i>
     </a>
