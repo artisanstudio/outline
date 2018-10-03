18e2ee9274f37f076be827c1310153095c36010a
Jaggy Gauran <jaggy@artisan.studio>
2018-10-03 15:32:58 +0800

    Add some comments.

--- config/outline.php
+++ config/outline.php
@@ -1,6 +1,14 @@
 <?php
 
 return [
-    'name' => 'Outline Admin',
-    'prefix' => 'admin',
+
+    /**
+     * The name that will be displayed in the admin sidebar.
+     */
+    'name' => config('app.name', 'Outline Admin'),
+
+    /**
+     * The prefix of the Outline routes.
+     */
+     'prefix' => 'admin',
 ];
