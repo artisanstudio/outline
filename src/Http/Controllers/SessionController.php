1d483be41ff895b8304da440296217d661a2189d
Jaggy Gauran <jaggy@artisan.studio>
2018-10-08 09:50:39 +0800

    Add the logout redirect to the config.

--- src/Http/Controllers/SessionController.php
+++ src/Http/Controllers/SessionController.php
@@ -4,10 +4,17 @@ namespace Artisan\Outline\Http\Controllers;
 
 class SessionController
 {
+    /**
+     * Sign the user out and redirect them out.
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function destroy()
     {
         auth()->logout();
 
-        return redirect('/');
+        return redirect()->to(
+            config('outline.logout_redirect')
+        );
     }
 }
